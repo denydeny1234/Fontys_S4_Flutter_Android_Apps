065ec76fe609cfbb488fa796b30a1c97a3d4ec12
Deny <denydeny1234@yahoo.com>
2024-04-19 16:00:27 +0200

    I bypass the need for the dart_openai package and directly set dummy values for the detected recipe details.

--- Tech_Case_12_Not_a_cookbook/recipes_app/lib/add_recipe_page.dart
+++ Tech_Case_12_Not_a_cookbook/recipes_app/lib/add_recipe_page.dart
@@ -1,7 +1,150 @@
-import 'dart:io';
+// import 'dart:io';
 
+// import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart'; // Import Image Picker
+// import 'database_helper.dart';
+// import 'recipe.dart';
+
+// class AddRecipePage extends StatefulWidget {
+//   @override
+//   _AddRecipePageState createState() => _AddRecipePageState();
+// }
+
+// class _AddRecipePageState extends State<AddRecipePage> {
+//   final _formKey = GlobalKey<FormState>();
+//   TextEditingController titleController = TextEditingController();
+//   TextEditingController ingredientsController = TextEditingController();
+//   TextEditingController instructionsController = TextEditingController();
+//   String? imagePath;
+
+//   final ImagePicker _picker = ImagePicker();
+
+//   Future getImage() async {
+//     // Show a dialog with options
+//     showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           title: Text("Choose Image Source"),
+//           content: SingleChildScrollView(
+//             child: ListBody(
+//               children: <Widget>[
+//                 GestureDetector(
+//                   child: Text("Camera"),
+//                   onTap: () {
+//                     _pickImage(ImageSource.camera);
+//                     Navigator.of(context).pop(); // Close the dialog
+//                   },
+//                 ),
+//                 Padding(padding: EdgeInsets.all(8.0)),
+//                 GestureDetector(
+//                   child: Text("Gallery"),
+//                   onTap: () {
+//                     _pickImage(ImageSource.gallery);
+//                     Navigator.of(context).pop(); // Close the dialog
+//                   },
+//                 ),
+//               ],
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+
+//   Future _pickImage(ImageSource source) async {
+//     final pickedFile = await _picker.pickImage(source: source);
+
+//     if (pickedFile != null) {
+//       setState(() {
+//         imagePath = pickedFile.path;
+//       });
+//     }
+//   }
+
+//   void addRecipe() async {
+//     print("Trying to add recipe");
+//     if (_formKey.currentState!.validate()) {
+//       print("Form is valid");
+//       Recipe newRecipe = Recipe(
+//         title: titleController.text,
+//         imagePath: imagePath ?? 'assets/images/default.jpg',
+//         ingredients: ingredientsController.text,
+//         instructions: instructionsController.text,
+//       );
+//       print("Recipe to add: ${newRecipe.toMap()}");
+//       await DatabaseHelper.instance.create(newRecipe);
+//       print("Recipe added");
+//       Navigator.pop(
+//           context); // This should take you back to the previous screen
+//     } else {
+//       print("Form is not valid");
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Add New Recipe'),
+//       ),
+//       body: Form(
+//         key: _formKey,
+//         child: ListView(
+//           padding: EdgeInsets.all(8),
+//           children: <Widget>[
+//             TextFormField(
+//               controller: titleController,
+//               decoration: InputDecoration(labelText: 'Title'),
+//               validator: (value) {
+//                 if (value == null || value.isEmpty) {
+//                   return 'Please enter a title';
+//                 }
+//                 return null;
+//               },
+//             ),
+//             TextFormField(
+//               controller: ingredientsController,
+//               decoration: InputDecoration(labelText: 'Ingredients'),
+//               validator: (value) {
+//                 if (value == null || value.isEmpty) {
+//                   return 'Please enter ingredients';
+//                 }
+//                 return null;
+//               },
+//             ),
+//             TextFormField(
+//               controller: instructionsController,
+//               decoration: InputDecoration(labelText: 'Instructions'),
+//               validator: (value) {
+//                 if (value == null || value.isEmpty) {
+//                   return 'Please enter instructions';
+//                 }
+//                 return null;
+//               },
+//             ),
+//             SizedBox(height: 10),
+//             imagePath == null
+//                 ? Text('No image selected.')
+//                 : Image.file(File(imagePath!)),
+//             ElevatedButton(
+//               onPressed: getImage,
+//               child: Text('Choose Image'),
+//             ),
+//             ElevatedButton(
+//               onPressed: addRecipe,
+//               child: Text('Add Recipe'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart'; // Import Image Picker
+import 'package:image_picker/image_picker.dart';
 import 'database_helper.dart';
 import 'recipe.dart';
 
@@ -20,7 +163,6 @@ class _AddRecipePageState extends State<AddRecipePage> {
   final ImagePicker _picker = ImagePicker();
 
   Future getImage() async {
-    // Show a dialog with options
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -59,26 +201,36 @@ class _AddRecipePageState extends State<AddRecipePage> {
       setState(() {
         imagePath = pickedFile.path;
       });
+
+      // Call a method to process the image and extract recipe details
+      detectRecipe(imagePath!);
     }
   }
 
+  Future<void> detectRecipe(String imagePath) async {
+    // Simulate parsing detected recipe details
+    // Here, we'll just set dummy values
+    final Map<String, String> recipeDetails = {
+      'title': 'Detected Recipe Title',
+      'ingredients': 'Ingredient 1, Ingredient 2, Ingredient 3',
+    };
+
+    // Update the title and ingredients controllers with the detected recipe details
+    titleController.text = recipeDetails['title'] ?? '';
+    ingredientsController.text = recipeDetails['ingredients'] ?? '';
+  }
+
   void addRecipe() async {
-    print("Trying to add recipe");
     if (_formKey.currentState!.validate()) {
-      print("Form is valid");
       Recipe newRecipe = Recipe(
         title: titleController.text,
         imagePath: imagePath ?? 'assets/images/default.jpg',
         ingredients: ingredientsController.text,
         instructions: instructionsController.text,
       );
-      print("Recipe to add: ${newRecipe.toMap()}");
+
       await DatabaseHelper.instance.create(newRecipe);
-      print("Recipe added");
-      Navigator.pop(
-          context); // This should take you back to the previous screen
-    } else {
-      print("Form is not valid");
+      Navigator.pop(context); // Navigate back to the previous screen
     }
   }
 
--- Tech_Case_12_Not_a_cookbook/recipes_app/lib/database_helper.dart
+++ Tech_Case_12_Not_a_cookbook/recipes_app/lib/database_helper.dart
@@ -70,7 +70,6 @@
 
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:path_provider/path_provider.dart';
 import 'recipe.dart';
 
 class DatabaseHelper {
